d50991eb1cc5446b2cc329fee4fc740e884dc054
Peter Erdodi <65724132+erdodip@users.noreply.github.com>
2022-07-19 12:06:26 +0200

    Update git_create_new_repo.sh

--- git_create_new_repo.sh
+++ git_create_new_repo.sh
@@ -7,3 +7,4 @@ git commit -m "First commit"
 git branch -M main
 git remote add origin git@github.com:erdodip/install.git
 git push -u origin main
+git pull origin main --allow-unrelated-histories # if it has some error during pull
